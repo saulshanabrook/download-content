497b841fb15251efcf0434639222519824f180ee
Saul <s.shanabrook@gmail.com>
2014-07-22 13:27:04 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import flask
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def main():
+    content = flask.request.args.get('content', '')
+    mimetype = flask.request.args.get('mimetype', 'application/octet-stream')
+    filename = flask.request.args.get('filename', 'file')
+    content_disposition = 'attachment; filename="{}"'.format(filename)
+
+    response = flask.Response(
+        content,
+        mimetype=mimetype
+    )
+    response.headers.add('Content-Disposition', content_disposition)
+    return response
+
+if __name__ == "__main__":
+    app.run()
